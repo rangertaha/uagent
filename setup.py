047146acbeead6af38807146529b15bc1b9a8b4b
Rangertaha <rangertaha@gmail.com>
2014-06-28 07:49:54 -0400

    Added updates

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(name='uagent', packages=['uagent'], version='0.0.1',
       description='Generates user agent strings. I wanted a user agent '
                   'generator that would cover a wide range of browser and '
-                  'device types.', author='Rangertaha',
+                  'device types. This tools allows you to select witch type of user agent string to generate.', author='Rangertaha',
       author_email='rangertaha@gmail.com',
-      url='https://github.com/rangertaha/uagent',
+      url='git@github.com:rangertaha/uagent.git',
       download_url='https://github.com/rangertaha/uagent/archive/master.zip',
       keywords=['user', 'agent', 'random', 'generator', 'category',
-                'u-agent categories'], classifiers=[], )
+                'u-agent categories'], classifiers=['Programming Language :: Python'], )
